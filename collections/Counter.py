baecac498bc17590b7d9f344eba1da2f3fea7e2c
yefeihonours <yefeihonour@foxmail.com>
2018-02-28 15:40:08 +0800

    crlf

--- collections/Counter.py
+++ collections/Counter.py
@@ -1,1 +1,41 @@
-import collections'''Counter是对字典类型的补充，用于追踪值的出现次数，具备字典的所有功能 + 自己的功能'''a='abababsbsbhh'c=collections.Counter(a) #直接列出每个元素出现了几次，传入列表和元组也一样print(c)#输出：Counter({'b': 5, 'a': 3, 'h': 2, 's': 2})#most_common 列出Counter内的前几个print(c.most_common())print(c.most_common(1))print(c.most_common(3))'''输出：[('b', 5), ('a', 3), ('h', 2), ('s', 2)][('b', 5)][('b', 5), ('a', 3), ('h', 2)]'''#update 相加c=collections.Counter(a)c1=collections.Counter(a)c.update(c1)print(c)#输出：Counter({'b': 10, 'a': 6, 'h': 4, 's': 4})#subtract 减aa=collections.Counter("as")bb=collections.Counter("htw")aa.subtract(bb) #aa-bbprint(aa)#输出：Counter({'a': 1, 's': 1, 'h': -1, 't': -1, 'w': -1})#elements  返回包含所有元素集合的迭代器for item in c.elements():    print(item, ' ', end='')
+import collections
+
+'''
+Counter是对字典类型的补充，用于追踪值的出现次数，具备字典的所有功能 + 自己的功能
+'''
+
+a='abababsbsbhh'
+
+c=collections.Counter(a) #直接列出每个元素出现了几次，传入列表和元组也一样
+print(c)
+#输出：Counter({'b': 5, 'a': 3, 'h': 2, 's': 2})
+
+#most_common 列出Counter内的前几个
+print(c.most_common())
+print(c.most_common(1))
+print(c.most_common(3))
+
+'''
+输出：
+[('b', 5), ('a', 3), ('h', 2), ('s', 2)]
+[('b', 5)]
+[('b', 5), ('a', 3), ('h', 2)]
+'''
+
+#update 相加
+c=collections.Counter(a)
+c1=collections.Counter(a)
+c.update(c1)
+print(c)
+#输出：Counter({'b': 10, 'a': 6, 'h': 4, 's': 4})
+
+#subtract 减
+aa=collections.Counter("as")
+bb=collections.Counter("htw")
+aa.subtract(bb) #aa-bb
+print(aa)
+#输出：Counter({'a': 1, 's': 1, 'h': -1, 't': -1, 'w': -1})
+
+#elements  返回包含所有元素集合的迭代器
+for item in c.elements():
+    print(item, ' ', end='')
--- collections/defaultdict.py
+++ collections/defaultdict.py
@@ -1,1 +1,7 @@
-import collections#默认字典defaultdict是对字典的类型的补充，他默认给字典的值设置了一个类型。dic=collections.defaultdict(list) #默认字典的value，也可以是元组(tuple)或字典(dict)#相当于dic={}dic["k1"]=[]
+import collections
+
+#默认字典defaultdict是对字典的类型的补充，他默认给字典的值设置了一个类型。
+
+dic=collections.defaultdict(list) #默认字典的value，也可以是元组(tuple)或字典(dict)
+#相当于dic={}
+dic["k1"]=[]
