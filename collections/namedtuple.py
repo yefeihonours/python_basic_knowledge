f1926046525c8ed5fc9c690906c278a58f29a6ec
yefeihonours <yefeihonour@foxmail.com>
2018-02-28 15:37:18 +0800

    test

--- collections/namedtuple.py
+++ collections/namedtuple.py
@@ -1,1 +1,19 @@
-import collections#tupled的扩展  可命名元组#创建一个普通tuplet=(1,2)#创建一个扩展tuple的类，MytupleMytuple=collections.namedtuple('Mytuples',['x','y'])tu=Mytuple(1,2) #相当于给原来的值赋了一个keyprint(t)print(tu)print(tu.x,tu.y)'''输出：(1, 2)Mytuples(x=1, y=2)2'''
+import collections
+
+#tupled的扩展  可命名元组
+
+#创建一个普通tuple
+t=(1,2)
+#创建一个扩展tuple的类，Mytuple
+Mytuple=collections.namedtuple('Mytuples',['x','y'])
+
+tu=Mytuple(1,2) #相当于给原来的值赋了一个key
+print(t)
+print(tu)
+print(tu.x,tu.y)
+'''
+输出：
+(1, 2)
+Mytuples(x=1, y=2)
+2
+'''
